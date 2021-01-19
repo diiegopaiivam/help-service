273063dfce496cbb6aabfa07778ee1932c723f98
Diego Paiva <diiegopaiivam@gmail.com>
2021-01-18 22:26:24 -0300

    crud de employeers

--- backend/app/controllers/api/v1/employees_controller.rb
+++ backend/app/controllers/api/v1/employees_controller.rb
@@ -19,6 +19,18 @@ class Api::V1::EmployeesController < Api::V1::ApiController
         end
     end
 
+    def update
+        if @employee.update(employee_params)
+            render json: @employee
+        else 
+            render json: @employee.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy 
+        @employee.destroy
+    end
+
     private 
         def employee_params
             params.require(:employee).permit(:first_name, :last_name, :email, :phone, :latitude, :longitude, :profession_id)
