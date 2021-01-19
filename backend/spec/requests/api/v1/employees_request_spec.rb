e8f3e7a6a89b85e5d979b5f3fc5e9861d0b71c7f
Diego Paiva <diiegopaiivam@gmail.com>
2021-01-18 22:56:16 -0300

    testando controllers

--- backend/spec/requests/api/v1/employees_request_spec.rb
+++ backend/spec/requests/api/v1/employees_request_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Employees", type: :request do
+    describe "GET /employeers" do
+        it "return success status" do
+            get api_v1_employees_path
+            expect(response).to have_http_status(200)
+        end
+    end
 
+    describe "POST /employees" do
+        context "when it has valid parameters" do
+            it "creates the employee with correct attributes" do
+                employee_attributes = FactoryBot.attributes_for(:employee)
+                post api_v1_employees_path, params: { employee: employee_attributes }
+                expect(Employee.last).to have_attributes(employee_attributes)
+            end
+        end
+    end
 end
